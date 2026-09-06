cf1b7287d89080333c2e7b1b1fa748386ed8ef9e
Moretti815 <kemiaomcy@outlook.com>
2026-09-06 12:55:24 +0800

    3.2.1

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -265,6 +265,14 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			showOnPostPage: true,
 		},
 		{
+			// 组件类型：欢迎时钟组件
+			type: "welcomeClock",
+			// 是否启用该组件
+			enable: true,
+			// 是否在文章详情页显示
+			showOnPostPage: true,
+		},
+		{
 			// 组件类型：分类组件
 			type: "categories",
 			// 是否启用该组件
